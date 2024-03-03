b0a435857e470f82e3c6b6121a04d097b8d1928c
luigitarallo <tarallo.gigi@gmail.com>
2024-03-03 12:03:40 +0100

    feat: add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
