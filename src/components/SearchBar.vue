f0e21bf5d953bad3675af13a1fdfce602cad12db
luigitarallo <tarallo.gigi@gmail.com>
2024-03-03 12:34:32 +0100

    feat: add search th emit to App

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      pokeToSearch: "",
+    };
+  },
+  methods: {
+    // Metodo per trimmare e mettere lowercase
+    // l'input, in modo da evitare errori
+    cleanPokeSearch() {
+      const pokeSearchClean = this.pokeToSearch.trim().toLowerCase();
+      this.$emit("searchPokemon", pokeSearchClean);
+    },
+  },
+  emits: ["searchPokemon"],
+};
+</script>
+
+<template>
+  <input
+    @keyup.enter="cleanPokeSearch"
+    v-model="pokeToSearch"
+    placeholder="Search a Pokémon!"
+  />
+  <button class="btn btn-outline-success" @click="cleanPokeSearch">
+    Search
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
